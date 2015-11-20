2459e498ed0c1ac357e9dbf43f4f093e249ac27a
Sylvain Chambon <sylvain.chambon@openwide.fr>
2015-11-20 15:24:50 +0100

    Ensure logout proceeds if token revocation fails

--- src/main/java/org/oasis_eu/spring/kernel/security/OasisLogoutHandler.java
+++ src/main/java/org/oasis_eu/spring/kernel/security/OasisLogoutHandler.java
@@ -44,33 +44,51 @@ public class OasisLogoutHandler implements LogoutSuccessHandler {
     @Override
     public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
 
-        request.getSession().invalidate();
-
-        // Okay, at this stage we have logged out from CK,
-        // we want to log out from Ozwillo too (otherwise it doesn't make sense to log out from CK)
+        // Okay, at this stage we have ended the Spring authentication,
+        // now we want to cleanly log out of Ozwillo
         if (authentication instanceof OpenIdCAuthentication) {
             OpenIdCAuthentication token = (OpenIdCAuthentication) authentication;
-            
-            // Direct Kernel API call kernel.exchange...
-            ResponseEntity<String> entity = kernel.exchange(
-            									configuration.getRevocationEndpoint(), 
-            									HttpMethod.POST, 
-            									this.createRevocationRequest(token),
-            									String.class, 
-            									client(configuration.getClientId(), configuration.getClientSecret() ) );
-            
-            if (entity.getStatusCode().value() != 200) { // RFC 7009 says the response should be 200 unless we provided an unknown token type
-                LOGGER.error("Cannot handle revocation response with code: " + entity.getStatusCode());
-                LOGGER.error("Payload is: " + entity.getBody() );
+
+            // 1. Revoke token
+            try {
+                // Direct Kernel API call kernel.exchange...
+                ResponseEntity<String> entity = kernel.exchange(
+                        configuration.getRevocationEndpoint(),
+                        HttpMethod.POST,
+                        this.createRevocationRequest(token),
+                        String.class,
+                        client(configuration.getClientId(), configuration.getClientSecret()));
+
+                if (entity.getStatusCode().value() != 200) { // RFC 7009 says the response should be 200 unless we provided an unknown token type
+                    LOGGER.error("Cannot handle revocation response with code: " + entity.getStatusCode());
+                    LOGGER.error("Payload is: " + entity.getBody());
+                }
+            } catch (Throwable t) {
+                // catch-all to be sure we proceed to invalidating the user session.
+                LOGGER.error("Cannot revoke access token", t);
             }
 
-            response.sendRedirect(UriComponentsBuilder.fromHttpUrl(logoutEndpoint)
-                    .queryParam("id_token_hint", token.getIdToken())
-                    .queryParam("post_logout_redirect_uri", afterLogoutUrl)
-                    .build()
-                    .toUriString());
+            // 2. to be sure we don't leak anything (though Spring should do the job), invalidate the session
+            request.getSession().invalidate();
+
+            // 3. redirect to the general Ozwillo logout page
+            UriComponentsBuilder logoutBuilder = UriComponentsBuilder.fromHttpUrl(logoutEndpoint)
+                    .queryParam("id_token_hint", token.getIdToken());
+            String kernelLogout;
+            if (afterLogoutUrl != null) {
+                kernelLogout = logoutBuilder
+                        .queryParam("post_logout_redirect_uri", afterLogoutUrl)
+                        .build()
+                        .toUriString();
+            } else {
+                kernelLogout = logoutBuilder.build().toUriString();
+            }
+
+            response.sendRedirect(kernelLogout);
 
         } else {
+            request.getSession().invalidate();
+
             String s = authentication != null ? authentication.getClass().toString() : "null";
             LOGGER.error("Authentication token " + s + " is not an OIDCAuthenticationToken; I don't know what to do with it");
 
@@ -81,6 +99,7 @@ public class OasisLogoutHandler implements LogoutSuccessHandler {
     private HttpEntity<MultiValueMap<String, String>> createRevocationRequest(OpenIdCAuthentication token){
     	MultiValueMap<String, String> revocationRequest = new LinkedMultiValueMap<>();
         revocationRequest.add("token", token.getAccessToken());
+        revocationRequest.add("token_type_hint", "access_token");
         
         // NB. HttpEntity has two constructors accepting one param. If a MultiValueMap is passed, it will 
 		// use the one that set the passed value IN the header. Here the Token is required to be set in the body.
--- src/main/java/org/oasis_eu/spring/kernel/security/OpenIdCConfiguration.java
+++ src/main/java/org/oasis_eu/spring/kernel/security/OpenIdCConfiguration.java
@@ -76,4 +76,6 @@ public interface OpenIdCConfiguration {
     default String getHomeUri() {
         return null;
     }
+
+
 }
